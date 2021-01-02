e4b65de2e32162bfc77c6ad2c70e2af44ada548e
roader6 <76880799+roader6@users.noreply.github.com>
2021-01-02 22:35:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var arr = ["Miesiąc",0,0,0,0,0,0,0,0,0,0,0,0,0];
 var arr1 = ["Średnia ocena telefonu w danym miesiącu",0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arr2 = ["Wartość sprzedaży",0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 fetch('https://my.api.mockaroo.com/ewd.json?key=b68b4930')
   .then(response => response.json())
@@ -10,17 +11,19 @@ fetch('https://my.api.mockaroo.com/ewd.json?key=b68b4930')
       for(var j=1;j<=12;j++){
         if(data[i].month == j){
           arr[j+1] = arr[j+1]+1;
-          arr1[j+1] = arr[j+1]+data[i].rating;
+          arr1[j+1] = arr1[j+1]+data[i].rating;
         }
       }
     };
-    for(i=1; i<=12;i++){
-      arr1.push(data[i].price)
+
+    for(j=2;j<=13;j++){
+      arr1[j] = arr1[j]/arr[j];
+      arr2[j] = arr[j]*1100;
     }
 
     console.log(arr);
     console.log(arr1);
-
+    console.log(arr2);
     
     var chart = bb.generate({
       bindto: "#chart",
@@ -36,12 +39,23 @@ fetch('https://my.api.mockaroo.com/ewd.json?key=b68b4930')
   var chart_2 = bb.generate({
     bindto: "#chart2",
     data: {
-        type: "bar",
+        type: "area",
         columns: [
             arr1
             //["data2", 130, 100, 140, 35, 110, 50]
         ]
     }
+});
+
+var chart = bb.generate({
+  bindto: "#chart3",
+  data: {
+      type: "spline",
+      columns: [
+          arr2
+          //["data2", 130, 100, 140, 35, 110, 50]
+      ]
+  }
 });
 
   });
