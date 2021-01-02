513d1b55e118b3eba59da8f746f845554f364711
roader6 <76880799+roader6@users.noreply.github.com>
2021-01-02 21:02:18 +0100

    new js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 fetch('https://my.api.mockaroo.com/ewd.json?key=364d7740')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data[0]);
+
+    
+  });
+
   
+var chart = bb.generate({
+    bindto: "#chart",
+    data: {
+        type: "bar",
+        columns: [
+            ["data1", 30, 200, 100, 170, 150, 250],
+            ["data2", 130, 100, 140, 35, 110, 50]
+        ]
+    }
+});
