b444b4464c45e7c1c9f0a303222801d753541628
roader6 <76880799+roader6@users.noreply.github.com>
2021-01-02 21:38:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-arr1 = [];
-arr2 = [];
+var arr1 = [];
+var arr2 = [];
 
 fetch('https://my.api.mockaroo.com/ewd.json?key=a57b9890')
   .then(response => response.json())
   .then(data => {
     
     console.log(data[0].month);
-    for(var i=5;i<=50;i++){
-      arr2.push("month");
+    arr2.push("month");
+    for(var i=0;i<=99;i++){
       arr2.push(data[i].month);
       arr1.push(arr2)
-    }
-    console.log(data);
+    };
 
     
+    var chart = bb.generate({
+      bindto: "#chart",
+      data: {
+          type: "bar",
+          columns: [
+              arr2,
+              ["data2", 130, 100, 140, 35, 110, 50]
+          ]
+      }
   });
 
-  
-var chart = bb.generate({
-    bindto: "#chart",
-    data: {
-        type: "bar",
-        columns: [
-            ["data1", 30, 200, 100, 170, 150, 250],
-            ["data2", 130, 100, 140, 35, 110, 50]
-        ]
-    }
-});
+  });
