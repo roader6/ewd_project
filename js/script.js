e4a9d27827086a6d0468326dc3976c9f91d2d96a
BarSolgaG-hub <68544264+BarSolgaG-hub@users.noreply.github.com>
2021-01-02 20:33:56 +0100

    2.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch('https://my.api.mockaroo.com/users.json?key=364d7740')
+fetch('https://my.api.mockaroo.com/ewd.json?key=364d7740')
   .then(response => response.json())
   .then(data => console.log(data));
   
