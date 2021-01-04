42e1d88be97f7d18c99925476a3327b6ebc52a5f
roader6 <76880799+roader6@users.noreply.github.com>
2021-01-04 11:01:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-var arr = ["Miesiąc",0,0,0,0,0,0,0,0,0,0,0,0,0];
-var arr1 = ["Średnia ocena telefonu w danym miesiącu",0,0,0,0,0,0,0,0,0,0,0,0,0];
-var arr2 = ["Wartość sprzedaży",0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arr = ["Miesiąc",0,0,0,0,0,0,0,0,0,0,0,0,0]; //chart1
+var arr1 = ["Średnia ocena telefonu w danym miesiącu",0,0,0,0,0,0,0,0,0,0,0,0,0]; ////chart2
+var arr2 = ["Wartość sprzedaży",0,0,0,0,0,0,0,0,0,0,0,0,0]; ////chart3
 
 fetch('https://my.api.mockaroo.com/ewd.json?key=b68b4930')
   .then(response => response.json())
   .then(data => {
-    
-    //console.log(data[0].month);
+    //100 rows indexed from 0 to 99
     for(var i=0;i<=99;i++){
       for(var j=1;j<=12;j++){
         if(data[i].month == j){
@@ -17,45 +16,36 @@ fetch('https://my.api.mockaroo.com/ewd.json?key=b68b4930')
     };
 
     for(j=2;j<=13;j++){
-      arr1[j] = arr1[j]/arr[j];
-      arr2[j] = arr[j]*1100;
+      arr1[j] = arr1[j]/arr[j]; //chart2
+      arr2[j] = arr[j]*1100; //chart3
     }
 
-    console.log(arr);
-    console.log(arr1);
-    console.log(arr2);
+    //console.log(arr);
+    //console.log(arr1);
+    //console.log(arr2);
     
-    var chart = bb.generate({
+    var chart = bb.generate({ //chart1
       bindto: "#chart",
       data: {
-          type: "bar",
-          columns: [
-              arr
-              //["data2", 130, 100, 140, 35, 110, 50]
-          ]
+        type: "bar",
+        columns: [arr]
       }
-  });
+    });
 
-  var chart_2 = bb.generate({
-    bindto: "#chart2",
-    data: {
+    var chart_2 = bb.generate({ //chart2
+      bindto: "#chart2",
+      data: {
         type: "area",
-        columns: [
-            arr1
-            //["data2", 130, 100, 140, 35, 110, 50]
-        ]
-    }
-});
+        columns: [arr1]
+      }
+    });
 
-var chart = bb.generate({
-  bindto: "#chart3",
-  data: {
-      type: "spline",
-      columns: [
-          arr2
-          //["data2", 130, 100, 140, 35, 110, 50]
-      ]
-  }
-});
+    var chart_3 = bb.generate({ //chart3
+      bindto: "#chart3",
+      data: {
+        type: "spline",
+        columns: [arr2]
+      }
+    });
 
-  });
+});
